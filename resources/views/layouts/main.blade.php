bb6edb3052c188c4b9c58b8ad8780a5e11f129c4
Chaerul Rizky <riz.chaerul@gmail.com>
2021-04-09 22:25:57 +0700

    fix styles

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">    
+    <link href="/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">    
     <link rel="icon" href="/logo.jpg">
 
     <title>Wonderful Journey</title>
@@ -62,5 +62,5 @@
     
     @yield('content')
 
-    <script src="/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>  </body>
+    <script src="/bootstrap.bundle.min.js" crossorigin="anonymous"></script>  </body>
 </html>
